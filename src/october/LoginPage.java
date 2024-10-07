0f85e5754b0b7981b7c3d9de77446d83611ede8c
manthanp1105 <48414838+manthanp1105@users.noreply.github.com>
2024-10-07 09:56:42 -0400

    Modified the classes

--- src/october/LoginPage.java
+++ src/october/LoginPage.java
@@ -3,12 +3,12 @@ package october;
 public class LoginPage {
 
 	public static void main(String[] args) {
-		
-		int a =10;
+
+		int a = 10;
 		int b = 20;
-		
-		System.out.println("Total of a & b is : "+(a+b));
-		System.out.println("Login page print statement");
+
+		System.out.println("Total of a & b is : " + (a + b));
+
 	}
 
 }
---
+++ src/october/PhineBook.java
@@ -0,0 +1,10 @@
+package october;
+
+public class PhineBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
