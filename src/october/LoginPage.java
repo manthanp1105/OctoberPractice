1718fc8334e5f9641c297f3e3ebfab7f72a10c9f
manthanp1105 <48414838+manthanp1105@users.noreply.github.com>
2024-10-07 09:37:39 -0400

    new class added

---
+++ src/october/LoginPage.java
@@ -0,0 +1,13 @@
+package october;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		
+		int a =10;
+		int b = 20;
+		
+		System.out.println("Total of a & b is : "+(a+b));
+	}
+
+}
